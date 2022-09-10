70fd0b926f57f1e49e39de224617ac7e4e18415f
AravindAK <102744383+Aravindaka14@users.noreply.github.com>
2022-09-10 14:19:18 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/postView' element={<PostView />}></Route>
         <Route path='/form' element={<Form />}></Route>
       </Routes>
