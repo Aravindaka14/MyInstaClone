a8e5e64097387f36995bd6466c25df8484ecef95
AravindAK <102744383+Aravindaka14@users.noreply.github.com>
2022-10-26 21:49:46 +0530

    Update postView.js

--- Server/routes/postView.js
+++ Server/routes/postView.js
@@ -4,6 +4,7 @@ const postModal = require("../modals/postSchema");
 const router = express.Router()
 
 router.get("/postView", (req, res) => {
+
     postModal.find().then((data) => {
         res.status(200).send(data)
     }).catch((err) => {
@@ -11,4 +12,13 @@ router.get("/postView", (req, res) => {
     })
 });
 
+router.put("/postView/update", async (req, res) => {
+    let like = await postModal.find({ _id: req.body.id })
+    // console.log(req.body)
+    like = like[0].likes + 1
+    await postModal.updateOne({ _id: req.body.id }, { likes: like })
+
+    res.status(200).send("likes updated")
+
+})
 module.exports = router;
