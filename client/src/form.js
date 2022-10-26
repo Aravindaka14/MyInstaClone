8dc4b187f335545cefaabdb9bbb941e40283682f
AravindAK <102744383+Aravindaka14@users.noreply.github.com>
2022-10-26 22:13:40 +0530

    Update form.js

--- client/src/form.js
+++ client/src/form.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./form.css"
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
 import FileBase64 from "react-file-base64";
@@ -8,30 +8,30 @@ import FileBase64 from "react-file-base64";
 const Form = () => {
     const navigate = useNavigate()
     const [post, setPost] = useState({})
-
     const handleData = async (e) => {
         e.preventDefault()
-        await axios({ method: "POST", url: "https://myinstaclone-02.herokuapp.com/form", data: post }).catch((err) => {
+        await axios({ method: "POST", url: "http://localhost:3005/form", data: post }).catch((err) => {
             console.log(err)
         }).finally(() => {
             navigate("/postView")
         })
+    }
+    const naviForm = () => {
+        navigate("/postView")
     }
     return (
         <div className="formDiv">
             <header>
                 <img className="nameLogo" src="nameLogo.svg" alt="" />
                 <span>Instaclone</span>
-                <Link to="/form">
-                    <img className="camera" src="camera.png" alt="" />
-                </Link>
+                <button className="camera" onClick={() => naviForm()}><i className="fa fa-camera"></i></button>
             </header>
             <form onSubmit={handleData}>
                 <FileBase64 type="file" onDone={(base64) => { setPost({ ...post, image: base64 }) }} required={true}></FileBase64>
                 <input type="text" placeholder="Author" onChange={(e) => { setPost({ ...post, name: e.target.value }) }} required={true} />
                 <input type="text" placeholder="Location" onChange={(e) => { setPost({ ...post, location: e.target.value }) }} required={true} />
                 <input type="text" placeholder="Description" onChange={(e) => { setPost({ ...post, description: e.target.value }) }} required={true} />
-                <button type="submit">Post</button>
+                <button className="form-btn" type="submit">Post</button>
             </form>
         </div>
     )
